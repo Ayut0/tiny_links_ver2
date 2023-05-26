c2c0bfcb1d6dd281d6114692da2be1482d0b1563
Yuto Yamakita <yuvto.0420@gmail.com>
2023-05-26 14:30:13 -0700

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { FirebaseAppProvider } from 'reactfire'
+
+import { firebaseConfig } from '@/lib/firebase'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return (
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Component {...pageProps} />;
+    </FirebaseAppProvider>
+  )
 }
